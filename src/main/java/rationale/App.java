36479d2e814e18a5eb0869e8e192c6fd0901d98f
Blake Dejohn <unknowingly@tamu.edu>
2024-03-07 10:43:09 -0600

    started project

---
+++ src/main/java/rationale/App.java
@@ -0,0 +1,13 @@
+package rationale;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
